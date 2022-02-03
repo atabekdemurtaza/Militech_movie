6b8287274b53f67c2f3eaa40256776e9c196579c
atabekdemurtaza <mychannel21war@gmail.com>
2022-02-04 03:11:59 +0500

    Second

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Movie 
+from django.views.generic.base import View
 
-# Create your views here.
+class MoviesView(View):
+
+	"""Список фильмов"""
+	def get(self, request):
+		movies = Movie.objects.all()
+		context = {
+			'movies' : movies 
+		}
+		return render(request, 'movies/movies.html', context)
+
